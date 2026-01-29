b083e25b7732a0c3e5f7f82055b33fba85f0286f
MarcusTL12 <marcuspaafjellet@gmail.com>
2026-01-29 15:17:03 +0100

    It works

--- cisd.c
+++ cisd.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 void cisd(size_t nocc, size_t nvir, double* data_1e, double* data_2e) {
-    
+    size_t nmo = nocc + nvir;
+
+    int pid = getpid();
+
+    char dirpath[128], filename_1e[128], filename_2e[128];
+
+    sprintf(dirpath, "/tmp/cisd%zu", pid);
+    sprintf(filename_1e, "%s/1e.dat", dirpath);
+    sprintf(filename_2e, "%s/2e.dat", dirpath);
+
+    mkdir(dirpath, 0755);
+
+    FILE* file_1e = fopen(filename_1e, "w");
+    fwrite(data_1e, sizeof(double), nmo * nmo, file_1e);
+    fclose(file_1e);
+
+    FILE* file_2e = fopen(filename_2e, "w");
+    fwrite(data_2e, sizeof(double), nmo * nmo * nmo * nmo, file_2e);
+    fclose(file_2e);
+
+    // Actually call the python interface
+
+    char command[256];
+    sprintf(command, "./.venv/bin/python cisd.py %zu %zu %s %s", nocc, nvir,
+            filename_1e, filename_2e);
+
+    system(command);
+
+    file_1e = fopen(filename_1e, "r");
+    fread(data_1e, sizeof(double), nmo * nmo, file_1e);
+    fclose(file_1e);
+
+    file_2e = fopen(filename_2e, "r");
+    fread(data_2e, sizeof(double), nmo * nmo * nmo * nmo, file_2e);
+    fclose(file_2e);
+
+    remove(filename_1e);
+    remove(filename_2e);
+    remove(dirpath);
 }
 
 int main() {
